bdc8282711e626a1f89d411b2495d1916258ecf8
Emiliano1919 <emilianojg38@gmail.com>
2025-03-21 18:40:22 -0400

    Write basic placement functionality

--- battleship.sh
+++ battleship.sh
@@ -1,4 +1,4 @@
-#/!/bin/bash
+#!/bin/bash
 tput clear;
 # Get terminal size
 height=$(tput lines);
@@ -69,35 +69,147 @@ read
 # Clear the terminal screen
 clear
 
-declare -a playerBoard
+declare -a player1Board
 declare -a player2Board
 numbers=(1 2 3 4 5 6 7 8 9 10)
 letters=(a b c d e f g h i j)
-
-printf "Player 1 Board \n \n"
-printf "     "
-for ((y=0; y<10; y++)); do
-    for ((x=0; x<10; x++)); do
-        playerBoard[(y*10)+x]="~";
+possibleDirections=(H V)
+declare -i playerTurn
+playerTurn=1
+initializeBoard(){
+    for ((y=0; y<10; y++)); do
+        for ((x=0; x<10; x++)); do
+            player1Board[(y*10)+x]="~";
+        done
     done
-done
-for ((x=0; x<10; x++)); do
-    printf "%c |  " "${numbers[$x]}"
-done
-printf "\n"
-printf "  " 
-for ((x=0; x<51; x++)); do
-    printf "_" 
-done
-printf "\n"
-for ((y=0; y<10; y++)); do
-    printf "%c |  " "${letters[$y]}"
+}
+printBoard(){
+    clear
+    printf "Player %u Board \n \n" "$1"
+    printf "     "
     for ((x=0; x<10; x++)); do
-        printf "%c |  " "${playerBoard[$((y * 10 + x))]}"
+        printf "%c |  " "${numbers[$x]}"
     done
     printf "\n"
-done
-printf "  " 
-for ((x=0; x<51; x++)); do
-    printf "-" 
-done
+    printf "  " 
+    for ((x=0; x<51; x++)); do
+        printf "_" 
+    done
+    printf "\n"
+    for ((y=0; y<10; y++)); do
+        printf "%c |  " "${letters[$y]}"
+        for ((x=0; x<10; x++)); do
+            printf "%c |  " "${player1Board[$((y * 10 + x))]}"
+        done
+        printf "\n"
+    done
+    printf "  " 
+    for ((x=0; x<51; x++)); do
+        printf "-" 
+    done
+}
+exists_in_list() {
+    local value="$1"
+    local list=("${@:2}")
+    for element in "${list[@]}"; do
+        if [ "$element" = "$value" ]; then
+            return 0
+        fi
+    done
+    return 1
+}
+
+actuallyPlaceShip(){
+    local shipType=$1
+    local shipLength=$2
+    local col=$3
+    local row=$4
+    local direction=$5
+    if [ "$direction" = "H" ]; then
+        for ((i=0; i<shipLength; i++)); do
+            player1Board[(row*10)+col+i]="$shipType";
+        done
+    fi
+
+    if [ "$direction" = "V" ]; then
+        for ((i=0; i<shipLength; i++)); do
+            player1Board[((row+i)*10)+col]="$shipType";
+        done
+    fi
+}
+
+placeShip(){
+    local shipType=$1
+    local shipLength=$2
+    local doneWithPlacement=0
+
+    while (( doneWithPlacement != 1 )); do
+        echo "Place your $shipType of length $shipLength"
+        read -p "Enter the starting column (i.e., a): " column
+        read -p "Enter the starting row (i.e., 2): " row
+        read -p "Enter the direction (H for horizontal, V for vertical): " direction
+
+        row=$((row-1)) #the board starts at 0 but we show it to the user as 1
+        col=$(( $(printf "%d" "'$column") - 97 ))  # Convert alphabetical column to 0-9 format
+        
+
+        if ! (exists_in_list "$column" "${letters[@]}"); then
+            printf "This is not a valid column"
+            continue
+        fi
+        if ! (exists_in_list "$row" "${numbers[@]}"); then
+            printf "This is not a valid row"
+            continue
+        fi
+        if ! (exists_in_list "$direction" "${possibleDirections[@]}"); then
+            printf "This is not a valid direction"
+            continue
+        fi
+
+        # Check for valid placement based on direction
+        validPlacement=true
+        if [ "$direction" = "H" ]; then
+            # Out of bounds check
+            if (( col + shipLength - 1 >= 10 )); then
+                validPlacement=false
+                printf "Ship cannot be placed horizontally, it extends beyond the board\n"
+            else
+                # Check if any of the spaces are already occupied
+                for ((i=0; i<shipLength; i++)); do
+                    if (( player1Board[(row*10)+(col+i)] != "~" )); then
+                        validPlacement=false
+                        printf "This position is already occupied\n"
+                        break
+                    fi
+                done
+            fi
+        elif [ "$direction" = "V" ]; then
+            # Out of bounds check
+            if (( row + shipLength - 1 >= 10 )); then
+                validPlacement=false
+                printf "Ship cannot be placed vertically, it extends beyond the board\n"
+            else
+                # Check if any of the spaces are already occupied
+                for ((i=0; i<shipLength; i++)); do
+                    if (( player1Board[((row+i)*10)+col] != "~" )); then
+                        validPlacement=false
+                        printf "This position is already occupied\n"
+                        break
+                    fi
+                done
+            fi
+        fi
+
+        if [ "$validPlacement" = true ]; then
+            actuallyPlaceShip "$shipType" "$shipLength" "$col" "$row" "$direction"
+            doneWithPlacement=1 
+        fi
+    done
+}
+initializeBoard
+printBoard $playerTurn
+actuallyPlaceShip "*" 4 1 1 "H"
+printBoard $playerTurn
+
+
+exists_in_list "a" "${letters[@]}"
